d36e591659e9a058a239d1956d84d453d63471cf
vivien <vivien.caux@outlook.fr>
2019-11-24 19:27:45 +0100

    Player movement(with jump) + deathZone

---
+++ Start3D/Assets/Scriptes/Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Déclaration de la variable de vitesse
+    public bool isGrounded;
+    public float m_speed = 0.1f;
+    public float j_speed = 10.0f;
+    private float canJump = 0f;
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+        if (col.gameObject.tag == "Death")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Création d'un nouveau vecteur de déplacement
+        Vector3 move = new Vector3();
+
+        if (Input.GetKey(KeyCode.Space) && Time.time > canJump)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (isGrounded)
+                {
+                    rb.AddForce(new Vector3(0, j_speed, 0), ForceMode.Impulse);
+                    isGrounded = false;
+                    canJump = Time.time + 0.5f;
+                }
+
+            }
+        }
+
+        // Récupération des touches haut et bas
+        if (Input.GetKey(KeyCode.UpArrow))
+            move.z += m_speed;
+        if (Input.GetKey(KeyCode.DownArrow))
+            move.z -= m_speed;
+
+        // Récupération des touches gauche et droite
+        if (Input.GetKey(KeyCode.LeftArrow))
+            move.x -= m_speed;
+        if (Input.GetKey(KeyCode.RightArrow))
+            move.x += m_speed;
+
+        // On applique le mouvement à l'objet
+        transform.position += move;
+    }
+}
